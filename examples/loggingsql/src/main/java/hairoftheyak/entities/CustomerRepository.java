816f0532bdb047830e5ea0212dad6fe3da4b5dca
Petter Remen <petter.remen@gmail.com>
2019-07-28 22:47:12 +0200

    Delete examples/loggingsql (move to separate repo)

--- examples/loggingsql/src/main/java/hairoftheyak/entities/CustomerRepository.java
+++
@@ -1,6 +0,0 @@
-package hairoftheyak.entities;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
-}
--- examples/loggingsql/src/main/resources/db/migration/V001__initial_setup.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE customers
-(
-    customer_id INT          NOT NULL AUTO_INCREMENT,
-    name        VARCHAR(255) NOT NULL,
-    PRIMARY KEY (customer_id)
-);
